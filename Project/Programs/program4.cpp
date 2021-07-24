c169d18646361495dd704a99ddac5664aeef3db7
ShreshthTiwari <shreshth.shreya24208@gmail.com>
2021-07-24 21:03:05 +0530

    e

--- Project/Programs/program4.cpp
+++ Project/Programs/program4.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
 
+using namespace std;
+
+int a;
+
+void Printing(){
+  cout<<a<<'\n';
+
+  int a = 5;
+
+  cout<<a<<'\n';
+}
+
+int main(){
+  a = 100;
+
+  Printing();
+
+  int a = 60;
+
+  cout<<a;
+
+  return 0;
+}
