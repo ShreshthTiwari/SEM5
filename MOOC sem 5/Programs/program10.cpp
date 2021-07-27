4c6bba26af4fc876efc1454999fa84f4320e8b46
ShreshthTiwari <shreshth.shreya24208@gmail.com>
2021-07-27 22:50:23 +0530

    e

---
+++ MOOC sem 5/Programs/program10.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+ifstream f("data10.in");
+ofstream g("data10.out");
+
+int main(){
+  int i, n, element, elementCount;
+
+  f>>n;
+
+  int arr[n];
+
+  for(i=0; i<=n-1; i++){
+    f>>arr[i];
+  }
+
+  f>>element;
+
+  elementCount = count(arr, arr+n, element);
+
+  if(elementCount > 0){
+    g<<"Element is present "<<count(arr, arr+n, element)<<" times.";
+  }else{
+    g<<"Element not present.";
+  }
+
+  return 0;
+}
