a666ee1b660f7234d869c7a7d710098162c40e9e
ShreshthTiwari <68736752+ShreshthTiwari@users.noreply.github.com>
2021-08-26 15:58:43 +0530

    e

---
+++ MOOC sem 5/Programs/program14.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void readArray(int *arr, int n){
+  for(int i=0; i<=n-1; i++){
+    cin>>arr[i];
+  }
+}
+
+int binarySearch(int *arr, int n, int element){
+  int left = 0, mid, right = n-1;
+  int i;
+
+  mid = (left + right) / 2;
+
+  if(element == arr[mid]) return mid;
+  else if(element < arr[mid])
+    right = mid-1;
+  else
+    left = mid+1;
+
+  for(i=left; i<=right; i++){
+    if(element == arr[i])
+      return i;
+  }  
+
+  return -1;
+}
+
+int main(){
+  int n;
+  int element;
+  int index;
+
+  cin>> n;
+
+  int arr[n];
+
+  readArray(arr, n);
+
+  cin>>element;
+
+  index = binarySearch(arr, n, element);
+
+  if(index > -1)
+    cout<<"Found "<<index;
+  else
+    cout<<"Not Found";  
+  cout<<'\n';
+  
+  return 0;
+}
